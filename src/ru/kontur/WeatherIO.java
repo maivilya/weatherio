94fbf937affb53fbfaa519ec7005a5c065e671d9
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-10-24 20:20:44 +0500

    added search text field and search button

--- src/ru/kontur/WeatherIO.java
+++ src/ru/kontur/WeatherIO.java
@@ -1,10 +1,16 @@
 package ru.kontur;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class WeatherIO extends JFrame {
 
     private static final String TITLE = "Погода";
+    private static final String FONT_FAMILY = "Roboto";
     private static final int WIDTH = 450;
     private static final int HEIGHT = 650;
 
@@ -15,5 +21,36 @@ public class WeatherIO extends JFrame {
         setLocationRelativeTo(null);
         setLayout(null);
         setResizable(false);
+        addComponents();
+    }
+
+    private void addComponents() {
+        addSearchField();
+        addSearchButton();
+    }
+
+    private void addSearchButton() {
+        JButton searchButton = new JButton(loadImage("src/ru/kontur/assets/search.png"));
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 15, 50, 45);
+        add(searchButton);
+    }
+
+    private void addSearchField() {
+        JTextField searchTextField = new JTextField();
+        searchTextField.setBounds(15, 15, 350, 45);
+        searchTextField.setFont(new Font(FONT_FAMILY, Font.PLAIN, 24));
+        add(searchTextField);
+    }
+
+    private ImageIcon loadImage(String resourcePath) {
+        try {
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+            return new ImageIcon(image);
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+        System.out.println("Could not find resource");
+        return null;
     }
 }
