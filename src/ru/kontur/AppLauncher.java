189c4de9ce99e08857f9eb9d58373e084ce7a28a
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-10-24 19:30:02 +0500

    set visible for WeatherIO

---
+++ src/ru/kontur/AppLauncher.java
@@ -0,0 +1,11 @@
+package ru.kontur;
+
+import javax.swing.*;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() ->
+                new WeatherIO().setVisible(true)
+        );
+    }
+}
