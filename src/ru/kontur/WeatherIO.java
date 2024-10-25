c2c1e0950bb56ddf79fc29c2cd09215601b89b71
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-10-25 18:18:13 +0500

    test response data from API to weather GUI

--- src/ru/kontur/WeatherIO.java
+++ src/ru/kontur/WeatherIO.java
@@ -1,8 +1,12 @@
 package ru.kontur;
 
+import org.json.simple.JSONObject;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,9 +16,19 @@ public class WeatherIO extends JFrame {
     private static final String TITLE = "Погода";
     private static final String FONT_FAMILY = "Roboto";
     private static final String SEARCH_RESOURCE_PATH = "src/ru/kontur/assets/search.png";
+    private static final String CLEAR_RESOURCE_PATH = "src/ru/kontur/assets/clear.png";
     private static final String CLOUDY_RESOURCE_PATH = "src/ru/kontur/assets/cloudy.png";
+    private static final String RAIN_RESOURCE_PATH = "src/ru/kontur/assets/rain.png";
+    private static final String SNOW_RESOURCE_PATH = "src/ru/kontur/assets/snow.png";
     private static final String HUMIDITY_RESOURCE_PATH = "src/ru/kontur/assets/humidity.png";
     private static final String WIND_SPEED_RESOURCE_PATH = "src/ru/kontur/assets/windspeed.png";
+    private static JTextField searchTextField;
+    private static JSONObject weatherData;
+    private JLabel weatherConditionImage;
+    private JLabel temperatureText;
+    private JLabel weatherDescription;
+    private JLabel humidityDescription;
+    private JLabel windSpeedDescription;
     private static final int WIDTH = 450;
     private static final int HEIGHT = 650;
 
@@ -41,7 +55,7 @@ public class WeatherIO extends JFrame {
     }
 
     private void addWindSpeedDescription() {
-        JLabel windSpeedDescription = new JLabel("<html><b>Wind speed</b> 3 km/h</html>");
+        windSpeedDescription = new JLabel("<html><b>Wind speed</b> 3 km/h</html>");
         windSpeedDescription.setBounds(310, 500, 85, 65);
         windSpeedDescription.setFont(new Font(FONT_FAMILY, Font.PLAIN, 18));
         add(windSpeedDescription);
@@ -54,7 +68,7 @@ public class WeatherIO extends JFrame {
     }
 
     private void addHumidityDescription() {
-        JLabel humidityDescription = new JLabel("<html><b>Humidity</b> 100%</html>");
+        humidityDescription = new JLabel("<html><b>Humidity</b> 100%</html>");
         humidityDescription.setBounds(90, 500, 85, 65);
         humidityDescription.setFont(new Font(FONT_FAMILY, Font.PLAIN, 18));
         add(humidityDescription);
@@ -67,7 +81,7 @@ public class WeatherIO extends JFrame {
     }
 
     private void addWeatherDescription() {
-        JLabel weatherDescription = new JLabel("Cloudy");
+        weatherDescription = new JLabel("Cloudy");
         weatherDescription.setBounds(0, 405, 450, 35);
         weatherDescription.setFont(new Font(FONT_FAMILY, Font.PLAIN, 32));
         weatherDescription.setHorizontalAlignment(SwingConstants.CENTER);
@@ -75,15 +89,15 @@ public class WeatherIO extends JFrame {
     }
 
     private void addTemperatureText(){
-        JLabel temperature = new JLabel("25º");
-        temperature.setBounds(0, 350, 450, 55);
-        temperature.setFont(new Font(FONT_FAMILY, Font.BOLD, 48));
-        temperature.setHorizontalAlignment(SwingConstants.CENTER);
-        add(temperature);
+        temperatureText = new JLabel("25º");
+        temperatureText.setBounds(0, 350, 450, 55);
+        temperatureText.setFont(new Font(FONT_FAMILY, Font.BOLD, 48));
+        temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(temperatureText);
     }
 
     private void addWeatherImage() {
-        JLabel weatherConditionImage = new JLabel(loadImage(CLOUDY_RESOURCE_PATH));
+        weatherConditionImage = new JLabel(loadImage(CLOUDY_RESOURCE_PATH));
         weatherConditionImage.setBounds(0, 125, 450, 220);
         add(weatherConditionImage);
     }
@@ -92,11 +106,40 @@ public class WeatherIO extends JFrame {
         JButton searchButton = new JButton(loadImage(SEARCH_RESOURCE_PATH));
         searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         searchButton.setBounds(375, 15, 50, 45);
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                String userCityInput = searchTextField.getText();
+                if (userCityInput.replaceAll("\\s", "").length() == 0) {
+                    return;
+                }
+                weatherData = WeatherApp.getWeatherData(userCityInput);
+                String weatherCondition = (String) weatherData.get("weatherCondition");
+                switch (weatherCondition) {
+                    case "Clear" -> weatherConditionImage.setIcon(loadImage(CLEAR_RESOURCE_PATH));
+                    case "Cloudy" -> weatherConditionImage.setIcon(loadImage(CLOUDY_RESOURCE_PATH));
+                    case "Rain" -> weatherConditionImage.setIcon(loadImage(RAIN_RESOURCE_PATH));
+                    case "Snow" -> weatherConditionImage.setIcon(loadImage(SNOW_RESOURCE_PATH));
+                }
+
+                double temperature = (double) weatherData.get("temperature");
+                temperatureText.setText(temperature + "º");
+
+                weatherDescription.setText(weatherCondition);
+
+                long humidity = (long) weatherData.get("humidity");
+                humidityDescription.setText("<html><b>Humidity</b> " + humidity + "%</html>");
+
+                double windSpeed = (double) weatherData.get("windSpeed");
+                windSpeedDescription.setText("<html><b>Wind speed</b> " + windSpeed + "km/h</html>");
+
+            }
+        });
         add(searchButton);
     }
 
     private void addSearchField() {
-        JTextField searchTextField = new JTextField();
+        searchTextField = new JTextField();
         searchTextField.setBounds(15, 15, 350, 45);
         searchTextField.setFont(new Font(FONT_FAMILY, Font.PLAIN, 24));
         add(searchTextField);
