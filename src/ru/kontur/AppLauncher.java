fa290fc86cc71753f28ab2959b4f4f173966878f
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-10-25 18:18:33 +0500

    make WeatherIO visible

--- src/ru/kontur/AppLauncher.java
+++ src/ru/kontur/AppLauncher.java
@@ -5,8 +5,8 @@ import javax.swing.*;
 public class AppLauncher {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() ->
-                //new WeatherIO().setVisible(true)
-                System.out.println(WeatherApp.getLocationData("Yekaterinburg"))
+                new WeatherIO().setVisible(true)
+
         );
     }
 }
