d936738f526d9bba0a1361b45b8bcf57b50998ef
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-10-25 09:28:34 +0500

    added WeatherApp and geolocation url

---
+++ src/ru/kontur/WeatherApp.java
@@ -0,0 +1,7 @@
+package ru.kontur;
+
+public class WeatherApp {
+
+    private static final String GEOLOCATION_URL = "https://geocoding-api.open-meteo.com/v1/search?name=%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3&count=10&language=ru&format=json";
+
+}
