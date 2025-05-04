a2332230298a9c25857dcf9ba4956b996ebfbac3
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2025-05-04 19:07:45 +0500

    added open weather map service

---
+++ src/ru/kontur/OpenWeatherMapService.java
@@ -0,0 +1,33 @@
+package ru.kontur;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class OpenWeatherMapService extends AbstractWeatherService{
+
+    private static final String OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric&lang=ru";
+    private static final String OPEN_WEATHER_MAP_API_KEY = "0fe00a1237c1788b7616dcf8ea456a26";
+    private static final JSONParser PARSER = new JSONParser();
+
+    public JSONObject getWeather(String locationName) {
+        String url = String.format(OPEN_WEATHER_MAP_URL, locationName, OPEN_WEATHER_MAP_API_KEY);
+        return fetchWeatherData(url);
+    }
+
+    @Override
+    protected JSONObject parseWeatherData(String stringJSON) throws Exception {
+        JSONObject resultJSONObj = (JSONObject) PARSER.parse(stringJSON);
+        JSONObject main = (JSONObject) resultJSONObj.get("main");
+        JSONObject wind = (JSONObject) resultJSONObj.get("wind");
+        JSONArray weatherArray = (JSONArray) resultJSONObj.get("weather");
+        JSONObject weather = (JSONObject) weatherArray.get(0);
+
+        JSONObject weatherData = new JSONObject();
+        weatherData.put("temperature", main.get("temp"));
+        weatherData.put("weatherCondition", weather.get("description"));
+        weatherData.put("humidity", main.get("humidity"));
+        weatherData.put("windSpeed", wind.get("speed"));
+        return weatherData;
+    }
+}
