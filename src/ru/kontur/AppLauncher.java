71a0593a93d2239a225dda418d74872a8ff9373f
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2025-05-11 00:08:47 +0500

    use new weather io

--- src/ru/kontur/AppLauncher.java
+++ src/ru/kontur/AppLauncher.java
@@ -1,11 +1,13 @@
 package ru.kontur;
 
+import ru.kontur.view.WeatherIO2;
+
 import javax.swing.*;
 
 public class AppLauncher {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() ->
-                new WeatherIO().setVisible(true)
+                new WeatherIO2().setVisible(true)
         );
     }
 }
