0a912b6f0c669a6433507c3dd76a01f725b99ab8
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2025-05-14 00:18:18 +0500

    added parse hourly forecast and test it(guess it work)

--- src/ru/kontur/model/service/weatherService/OpenWeatherMapService.java
+++ src/ru/kontur/model/service/weatherService/OpenWeatherMapService.java
@@ -8,11 +8,43 @@ public class OpenWeatherMapService extends AbstractWeatherService {
 
     private static final String OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric&lang=ru";
     private static final String OPEN_WEATHER_MAP_API_KEY = "0fe00a1237c1788b7616dcf8ea456a26";
+    private static final String OPEN_WEATHER_HOURLY_URL = "http://api.openweathermap.org/data/2.5/forecast?q=%s&appid=%s&units=metric&lang=ru";
     private static final JSONParser PARSER = new JSONParser();
 
     public JSONObject getWeather(String locationName) {
         String url = String.format(OPEN_WEATHER_MAP_URL, locationName, OPEN_WEATHER_MAP_API_KEY);
         return fetchWeatherData(url);
+    }
+
+    public JSONObject getHourlyForecast(String locationName) {
+        String url = String.format(OPEN_WEATHER_HOURLY_URL, locationName, OPEN_WEATHER_MAP_API_KEY);
+        return fetchHourlyForecastData(url);
+    }
+
+    @Override
+    protected JSONObject parseHourlyForecast(String jsonString) throws Exception {
+        JSONObject resultJSONObj = (JSONObject) PARSER.parse(jsonString);
+        JSONArray list = (JSONArray) resultJSONObj.get("list");
+
+        JSONArray hourlyForecast = new JSONArray();
+        for (Object obj : list) {
+            JSONObject forecastEntry = (JSONObject) obj;
+            JSONObject main = (JSONObject) forecastEntry.get("main");
+            JSONObject wind = (JSONObject) forecastEntry.get("wind");
+            JSONArray weatherArray = (JSONArray) forecastEntry.get("weather");
+            JSONObject weather = (JSONObject) weatherArray.get(0);
+
+            JSONObject hourData = new JSONObject();
+            hourData.put("time", forecastEntry.get("dt_txt"));
+            hourData.put("temperature", main.get("temp"));
+            hourData.put("weatherCondition", weather.get("description"));
+            hourData.put("humidity", main.get("humidity"));
+            hourData.put("windSpeed", wind.get("speed"));
+            hourlyForecast.add(hourData);
+        }
+        JSONObject result = new JSONObject();
+        result.put("hourly", hourlyForecast);
+        return result;
     }
 
     @Override
