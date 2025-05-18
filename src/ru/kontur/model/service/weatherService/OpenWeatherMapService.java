20b8a70faa7dfb640aef916dd248b7e1d1f683cc
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2025-05-18 17:27:42 +0500

    added methods to display correct temperature per hour

--- src/ru/kontur/model/service/weatherService/OpenWeatherMapService.java
+++ src/ru/kontur/model/service/weatherService/OpenWeatherMapService.java
@@ -4,6 +4,10 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class OpenWeatherMapService extends AbstractWeatherService {
 
     private static final String OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric&lang=ru";
@@ -25,7 +29,6 @@ public class OpenWeatherMapService extends AbstractWeatherService {
     protected JSONObject parseHourlyForecast(String jsonString) throws Exception {
         JSONObject resultJSONObj = (JSONObject) PARSER.parse(jsonString);
         JSONArray list = (JSONArray) resultJSONObj.get("list");
-
         JSONArray hourlyForecast = new JSONArray();
         for (Object obj : list) {
             JSONObject forecastEntry = (JSONObject) obj;
@@ -42,8 +45,9 @@ public class OpenWeatherMapService extends AbstractWeatherService {
             hourData.put("windSpeed", wind.get("speed"));
             hourlyForecast.add(hourData);
         }
+        JSONArray interpolated = interpolateHourlyData(hourlyForecast);
         JSONObject result = new JSONObject();
-        result.put("hourly", hourlyForecast);
+        result.put("hourly", interpolated);
         return result;
     }
 
@@ -61,5 +65,54 @@ public class OpenWeatherMapService extends AbstractWeatherService {
         weatherData.put("humidity", main.get("humidity"));
         weatherData.put("windSpeed", wind.get("speed"));
         return weatherData;
+    }
+
+    private JSONArray interpolateHourlyData(JSONArray rawForecast) throws Exception {
+        JSONArray interpolated = new JSONArray();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Calendar cal = Calendar.getInstance();
+
+        for (int i = 0; i < rawForecast.size() - 1; i++) {
+            JSONObject current = (JSONObject) rawForecast.get(i);
+            JSONObject next = (JSONObject) rawForecast.get(i + 1);
+
+            interpolated.add(current);
+
+            String time1 = (String) current.get("time");
+            String time2 = (String) next.get("time");
+
+            Date date1 = formatter.parse(time1);
+            Date date2 = formatter.parse(time2);
+
+            long hoursBetween = (date2.getTime() - date1.getTime()) / (1000 * 60 * 60);
+            if (hoursBetween > 1) {
+                double temp1 = toDouble(current.get("temperature"));
+                double temp2 = toDouble(next.get("temperature"));
+                for (int h = 1; h < hoursBetween; h++) {
+                    cal.setTime(date1);
+                    cal.add(Calendar.HOUR_OF_DAY, h);
+                    String interpolatedTime = formatter.format(cal.getTime());
+
+                    double factor = (double) h / hoursBetween;
+                    double interpolatedTemp = roundTwoDecimals(temp1 + (temp2 - temp1) * factor);
+
+                    JSONObject interpolatedObj = new JSONObject();
+                    interpolatedObj.put("time", interpolatedTime);
+                    interpolatedObj.put("temperature", interpolatedTemp);
+                    interpolatedObj.put("interpolated", true);
+                    interpolated.add(interpolatedObj);
+                }
+            }
+        }
+        interpolated.add(rawForecast.get(rawForecast.size() - 1));
+        return interpolated;
+    }
+
+    private double toDouble(Object val) {
+        return val instanceof Number ? ((Number) val).doubleValue() : Double.parseDouble(val.toString());
+    }
+
+    private double roundTwoDecimals(double value) {
+        return Math.round(value * 100.0) / 100.0;
     }
 }
